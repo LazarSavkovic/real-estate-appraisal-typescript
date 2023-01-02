c674807de58b76ef63627ac88c5da174d8843513
Lazar Savkovic <savkoviclazar1@gmail.com>
2023-01-02 22:11:36 +0100

    added the seed route in api

--- pages/api/apts/seed.ts
+++ pages/api/apts/seed.ts
@@ -1,30 +1,35 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import { connect } from "../../../utils/connection"
 import { ResponseFuncs } from "../../../utils/types"
-import seedApts from "../../../utils/seed"
-import {printScreen} from "../../../util/seed_apt"
-
-printScreen()
+import Apt from "models/Apt"
+import apartments from '../../../utils/seed_apts/apartments.json'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  //capture request method, we type it as a key of ResponseFunc to reduce typing later
-  const method: keyof ResponseFuncs = req.method as keyof ResponseFuncs
+    //capture request method, we type it as a key of ResponseFunc to reduce typing later
+    const method: keyof ResponseFuncs = req.method as keyof ResponseFuncs
 
-  //function for catch errors
-  const catcher = (error: Error) => res.status(400).json({ error })
+    //function for catch errors
+    const catcher = (error: Error) => res.status(400).json({ error })
 
-  // Potential Responses
-  const handleCase: ResponseFuncs = {
-    // RESPONSE POST REQUESTS
-    POST: async (req: NextApiRequest, res: NextApiResponse) => {
-      await seedApts().catch(catcher)
-    },
-  }
+    // Potential Responses
+    const handleCase: ResponseFuncs = {
+        // RESPONSE POST REQUESTS
+        POST: async (req: NextApiRequest, res: NextApiResponse) => {
+            console.log('before connection')
+            await connect();
 
-  // Check if there is a response for the particular method, if so invoke it, if not response with an error
-  const response = handleCase[method]
-  if (response) response(req, res)
-  else res.status(400).json({ error: "No Response for This Request" })
+            await Apt.deleteMany({});
+            await Apt.insertMany(apartments);
+            const apts = await Apt.find({})
+            console.log('inserted all apartments')
+            res.send("inserted all apartments")
+        },
+    }
+
+    // Check if there is a response for the particular method, if so invoke it, if not response with an error
+    const response = handleCase[method]
+    if (response) response(req, res)
+    else res.status(400).json({ error: "No Response for This Request" })
 }
 
 export default handler
